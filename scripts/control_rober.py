3b5104cdd32c39934c34b49d2e6d383b824d15f6
Miura55 <acordion.piano@gmail.com>
2022-01-15 16:26:25 +0000

    センサー連携のコードの追加とテスト用コードの削除

--- scripts/control_rober.py
+++ scripts/control_rober.py
@@ -5,12 +5,14 @@ from rospy.exceptions import ROSInterruptException
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Range
 
+
 class Contorller:
     def __init__(self):
         rospy.init_node('controller', anonymous=True)
-        self.is_forward = True
+        rospy.on_shutdown(self.shutdown_callback)
+        self.distance = 100
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        self.sub = rospy.Subscriber('scan', Range, self.callback)
+        self.sub = rospy.Subscriber('scan', Range, self.scan_callback)
         rospy.Timer(rospy.Duration(1.0), self.timer_callback)
 
         # Twistの初期化
@@ -19,24 +21,33 @@ class Contorller:
         twiet.angular.z = 0
         self.pub.publish(twiet)
 
-    def callback(self, data):
+    def scan_callback(self, data):
         # センサーの値を受け取る
-        distance = data.range
-        rospy.loginfo("distance: %f", distance)
+        self.distance = data.range
+        rospy.loginfo("distance: %f", self.distance)
 
     def timer_callback(self, event):
         # Twistの初期化
         twiet = Twist()
-        if self.is_forward:
-            twiet.linear.x = 0.5
+        if self.distance < 15:
+            twiet.linear.x = 0
+            twiet.angular.z = 0.5
         else:
-            twiet.linear.x = -0.5
-        self.pub.publish(twiet)
+            twiet.linear.x = 0.5
+            twiet.angular.z = 0
         rate = rospy.Rate(10)
-        rate.sleep()
+        for i in range(5):
+            self.pub.publish(twiet)
+            rate.sleep()
+
+    def shutdown_callback(self):
+        rospy.loginfo("Stop")
+        rospy.sleep(1)
+        twiet = Twist()
         twiet.linear.x = 0
+        twiet.angular.z = 0
         self.pub.publish(twiet)
-        self.is_forward = not self.is_forward
+
 
 if __name__ == '__main__':
     try:
