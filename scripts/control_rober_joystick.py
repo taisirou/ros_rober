8bfc31f48aecf5df9b48afbe9606cc2eaadaee86
Miura55 <acordion.piano@gmail.com>
2022-04-06 01:02:47 +0900

    Add joystick control launch

--- scripts/control_rober_joystick.py
+++ scripts/control_rober_joystick.py
@@ -8,14 +8,29 @@ from geometry_msgs.msg import Twist
 class Contorller:
     def __init__(self):
         rospy.init_node('controller', anonymous=True)
+        self.linear_x = 0
+        self.angular_zr = 0
+        self.angular_zl = 0
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.sub = rospy.Subscriber('joy', Joy, self.joy_callback)
         rospy.on_shutdown(self.shutdown_callback)
+        rospy.Timer(rospy.Duration(1.0), self.timer_callback)
     
     def joy_callback(self, joy_msg):
-        axes = joy_msg.axes
-        buttons = joy_msg.buttons
-        rospy.loginfo("axes: %s", axes)
-        rospy.loginfo("buttons: %s", buttons)
+        # Get Joy Event
+        self.linear_x = joy_msg.axes[1]
+        self.angular_zr = joy_msg.buttons[1]
+        self.angular_zl = joy_msg.buttons[3]
+
+    def timer_callback(self, event):
+        twist = Twist()
+        twist.linear.x = self.linear_x * 0.5
+        twist.angular.z = (self.angular_zr - self.angular_zl) * 0.5
+        rate = rospy.Rate(10)
+        for i in range(5):
+            self.pub.publish(twist)
+            rospy.loginfo("linear_x: %f, angular_z: %f", twist.linear.x, twist.angular.z)
+            rate.sleep()
 
     def shutdown_callback(self):
         rospy.loginfo("Stop")
@@ -24,6 +39,7 @@ class Contorller:
 
 if __name__ == '__main__':
     try:
+        rospy.loginfo("Start")
         Contorller()
         rospy.spin()
     except ROSInterruptException:
